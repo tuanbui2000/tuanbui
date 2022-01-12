192c055d887abe9d36d1908277c7b12279ee30e8
トゥアン <211138@st.yoshida-g.ac.jp>
2022-01-12 09:23:35 +0900

    php

--- php/history.php
+++ php/history.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>
-      history
+        history
     </title>
     <meta charset="ulf-8">
     <link rel="stylesheet" href="../css/menu.css">
@@ -11,23 +11,29 @@
 
 
 <body>
-    <form action="" method="POST">
+<form action="" method="POST">
         <div class="container">
-           <?php $title='<h1>履歴</h1>';
-           include 'ltm.php'?>
+            <?php $title = '<h1>管理</h1>';
+            include 'ltm.php' ?>
 
-            <div class="item" ></div>
+            <div class="item">
+                
+            </div>
 
-
-                <div class="item">
-
-                </div>
-
-
-
+            <!-- menu    -->
+            <div class="item">
+               <?php
+                $sql_syntax='select * from orders';
+                include 'dbconnect.php';
+              
+               ?> 
 
             </div>
 
+
+
+
+        </div>
 
     </form>
 </body>
--- php/menu.php
+++ php/menu.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -22,6 +26,10 @@
 
             <div class="item">
                 <?php
+                //confirm button process
+                if (isset($_POST["confirm-button"])) {
+                    header("location:order_confirm.php");
+                }
                 try {
                     // database connect
                     $db = new PDO('mysql:host=localhost;dbname=kushitori;charset=utf8', 'root', '');
@@ -39,80 +47,162 @@
 
 
 
-
-
-
-
-
+                    //xử lí đồ  đã chọn thì dùng style trực tiếp
 
 
                     $stmt = $db->prepare($sql_menu);
                     $stmt->execute();
 
                     while ($menu = $stmt->fetch(PDO::FETCH_NUM)) {
-                    echo " <input class='extra' type='button' value='$menu[0]'>";
-                    
+                        echo " <input class='extra' type='button' value='$menu[0]'>";
                     }
 
                 ?>
-
-                    <input class='extra' type="button" value="chicken">
-                    <input class='extra' type="button" value="minced chicken">
-                    <input class='extra' type="button" value="pork">
-                    <input class='extra' type="button" value="beff">
-                    <input class='extra' type="button" value="a la carte">
-                    <input class='extra' type="button" value="vegetable">
-                    <input class='extra' type="button" value="一品・サラダ・お食事">
-                    <input class='extra' type="button" value="dessert(デザート)">
-                    <!-- <input class='extra' type="button" value="beer / sake">
-            <input class='extra' type="button" value="shochu">
-            <input class='extra' type="button" value="frui liquor / wine">
-            <input class='extra' type="button" value="high ball">
-            <input class='extra' type="button" value="shochu by the bottle">
-            <input class='extra' type="button" value="割りもの">
-            <input class='extra' type="button" value="sours">
-            <input class='extra' type="button" value="cocktails">
-            <input class='extra' type="button" value="softdrink">
-            <input class='extra' type="button" value="fresh fruit sours"> -->
             </div>
 
 
             <div class="item">
-                <!-- <div class="menu"> -->
 
-            <?php
+                <div class="wrapper">
+
+                <?php
 
                     $stmt = $db->prepare($sql);
                     $stmt->execute();
                     //display data
                     while ($data = $stmt->fetch(PDO::FETCH_NUM)) {
-                        echo "<button style='background-image: url(../images/products/$data[0].jpg);'>
+                        echo "<button tupe='submit' value='$data[0]' style='background-image: url(../images/products/$data[0].jpg);' id=$data[0]  name='food'>
                             <h2> $data[1]</h2>
                              <hr>
-                             <h3> $data[2]¥</h3>
+                             <h2> $data[2]¥</h2>
                              </button>";
                     }
+
+
+
+                    // selected process
+                    if (isset($_POST['food'])) {
+                        $order = $_POST['food'];
+
+
+
+                        // to 5th 
+                        if (!isset($_SESSION["name1"])) {
+                            $_SESSION["name1"] = $order;
+                            $_SESSION["value1"] = 0;
+                        } else {
+                            if (!isset($_SESSION["name2"]) && $_SESSION["name1"] != $order) {
+                                $_SESSION["name2"] = $order;
+                                $_SESSION["value2"] = 0;
+                            } else {
+                                if (!isset($_SESSION["name3"]) && $_SESSION["name2"] != $order && $order != $_SESSION["name1"]) {
+                                    $_SESSION["name3"] = $order;
+                                    $_SESSION["value3"] = 0;
+                                } else {
+                                    if (!isset($_SESSION["name4"]) && $_SESSION["name3"] != $orderr && $order != $_SESSION["name1"] && $order != $_SESSION["name2"]) {
+                                        $_SESSION["name4"] = $order;
+                                        $_SESSION["value4"] = 0;
+                                    } else {
+                                        if (!isset($_SESSION["name5"]) && $_SESSION["name4"] != $orderr && $order != $_SESSION["name1"] && $order != $_SESSION["name2"] && $order != $_SESSION["name3"]) {
+                                            $_SESSION["name5"] = $order;
+                                            $_SESSION["value5"] = 0;
+                                        } else {
+
+                                            // alert
+                                            echo "full";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
+
+
+                        //
+
+
+                        //if order have been exist
+                        if ($_SESSION["name1"] == $order) {
+                            $_SESSION["value1"] += 1;
+                        } elseif ($_SESSION["name2"] == $order) {
+                            $_SESSION["value2"] += 1;
+                        } elseif ($_SESSION["name3"] == $order) {
+                            $_SESSION["value3"] += 1;
+                        } elseif ($_SESSION["name4"] == $order) {
+                            $_SESSION["value4"] += 1;
+                        } elseif ($_SESSION["name5"] == $order) {
+                            $_SESSION["value5"] += 1;
+                        } else {
+                            //alert
+                            echo "fiuc";
+                        }
+                    }
+
+
+
+
+
+
+
+
+
                     $db = null;
                 } catch (PDOException $e) {
                     print('database not connected ' . $e->getMessage());
                 } catch (Exception $e) {
                     print('予期せぬerorr ' . $e->getMessage());
                 }
-            ?>
+                ?>
+                </div>
+
+
+
+
+
+                <div class="confirm">
+
+
+                    <?Php
+                    //order
+                    if (isset($_SESSION["name1"])) {
+                        echo " <div class='selected'><img src='../images/products/" . $_SESSION['name1'] . ".jpg' > " . $_SESSION['value1'] . "</div> ";
+                    }
+                    if (isset($_SESSION["name2"])) {
+                        echo " <div class='selected'><img src='../images/products/" . $_SESSION['name2'] . ".jpg' > " . $_SESSION['value2'] . "</div> ";
+                    }
+                    if (isset($_SESSION["name3"])) {
+                        echo " <div class='selected'><img src='../images/products/" . $_SESSION['name3'] . ".jpg' > " . $_SESSION['value3'] . "</div> ";
+                    }
+                    if (isset($_SESSION["name4"])) {
+                        echo " <div class='selected'><img src='../images/products/" . $_SESSION['name4'] . ".jpg' > " . $_SESSION['value4'] . "</div> ";
+                    }
+                    if (isset($_SESSION["name5"])) {
+                        echo " <div class='selected'><img src='../images/products/" . $_SESSION['name5'] . ".jpg' > " . $_SESSION['value5'] . "</div> ";
+                    }
+
+                    //confirm button
+                    if (isset($_SESSION['name1'])) {
+                        echo '<input type="submit" name="confirm-button" value="確認">';
+                    }
+
+
+                    ?>
+
+
+
+
+                </div>
             </div>
-            <!-- <div class="confirm"> 
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse architecto quae, asperiores nostrum totam expedita, vel saepe quasi, alias officia molestias? Architecto ipsa repellat temporibus facilis quos expedita atque vero?
-                    <input type="submit" value="確認">
-                </div> -->
-        </div>
-
-
-
 
         </div>
+
+
 
 
     </form>
+
+
 </body>
 
 </html>
